c3fb340684f4ec124ed03ef3e3cbe21db3907a9c
Gerardo Nava Pereda <gerardo.nava@gnp.com.mx>
2020-03-10 22:42:00 -0600

    👌 Added Autocomplete Component to App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import client from "axios";
+import Autocomplete from "./components/Autocomplete";
+import './App.css'
 
 function App() {
+  const [data, setData] = useState([])
+  const [refresh, setRefresh] = useState(1)
+  const [openMessage, setOpenMessage] = useState(false)
+
+  useEffect(() => {
+    client("https://api.github.com/repos/facebook/react/issues")
+      .then(res => {
+        setData(res.data);
+      });
+  }, [refresh])
+
+  const handleRefresh = () => {
+    setRefresh(refresh + 1)
+    setOpenMessage(true)
+    setTimeout(() => {
+      setOpenMessage(false)
+    }, 5000)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {openMessage && <div className="App-message"><p>Lista de Issues actualizada!</p></div>}
+      <div className="App-container">
+        <div>
+          <h1 className="App-title">Autocomplete App</h1>
+          <Autocomplete data={data} />
+          <button className="App-button" onClick={handleRefresh}>Actualizar Issues</button>
+        </div>
+      </div>
     </div>
   );
 }
